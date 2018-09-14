7cd4210d81af987dbf5c69cec53f2155f82501eb
denglulu <denglulu@qianshengqian.com>
2018-09-14 19:03:48 +0800

    tijiao

--- src/main/java/com/autotest/demo/FirstDemo.java
+++ src/main/java/com/autotest/demo/FirstDemo.java
@@ -1,23 +1,26 @@
 package com.autotest.demo;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.FirefoxProfile;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 public class FirstDemo {
 
     @Test
-    public void test(){
-//        System.setProperty("webdriver.gecko.driver", "/Users/denglulu/Downloads/chromedriver");
+    public void test() {
+        System.setProperty("webdriver.chrome.driver", "/Users/denglulu/Downloads/chromedriver");
 //        WebDriver driver = new FirefoxDriver();
-//        driver.get("http://www.baidu.com");
 
 
-        FirefoxProfile profile = new FirefoxProfile();
-        // profile.EnableNativeEvents = true;
+//        System.setProperty("webdriver.gecko.driver", "/Users/denglulu/Downloads/geckodriver 3");
 
-        WebDriver driver = new FirefoxDriver(profile);
-        driver.get("http://www.baidu.com");
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://test3caiwu.back.so/");
+        driver.findElement(By.id("loginform-username")).sendKeys("18721356120");
+        driver.findElement(By.id("loginform-password")).sendKeys("123456");
+        driver.findElement(By.name("login-button")).click();
+
     }
+
 }
